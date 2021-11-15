397ef9ab0b88567693fcd86a1b0e6370403921d6
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-11-15 14:56:25 -0600

    Added more tests

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -2,8 +2,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert. assertNotNull;
 
 public class StudentTest {
+
+    @Before
+    public void setUp() {
+        Student rhiannon = new Student(9, "Rhiannon");
+        assertNotNull(rhiannon);
+    }
 
 
     @Test
